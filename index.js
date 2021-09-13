b0d0ed21e707400410b1384f59ea275b77fc4f7a
EddieSilveira <eddie_aparecido@hotmail.com>
2021-09-12 22:23:22 -0300

    Implementada as rotas de autenticacao e o token

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ InicializaMongoServer();
 
 const app = express();
 const PORT = process.env.PORT || 4000;
+const jwt = require('jsonwebtoken');
+const tokenSecret = process.env.SECRET;
 
 const rotasUsuario = require('./routes/Usuario');
 
@@ -26,11 +28,49 @@ app.use(function (req, res, next) {
 app.disable('x-powered-by');
 app.use(express.json());
 
+//TOKEN JWT
+function verificaJWT(req, res, next) {
+  const token = req.headers['x-access-token'];
+  if (!token)
+    return res.status(401).json({ auth: false, message: 'No token provided.' });
+
+  jwt.verify(token, tokenSecret, function (err, decoded) {
+    if (err)
+      return res
+        .status(500)
+        .json({ auth: false, message: 'Failed to authenticate token' });
+
+    req.userId = decoded.id;
+    next();
+  });
+}
+
+//Autenticação
+app.post('/signin', async (req, res, next) => {
+  const usuarios = await Usuario.find();
+  usuarios.forEach((usuario) => {
+    if (req.body.email === usuario.email && req.body.senha === usuario.senha) {
+      const id = usuario._id;
+      const token = jwt.sign({ id }, tokenSecret, {
+        expiresIn: 999,
+      });
+
+      return res.json({ auth: true, token: token, usuario: usuario });
+    } else {
+      return res.status(500).json({ message: 'Login Inválido' });
+    }
+  });
+});
+
+app.post('/signout', function (req, res) {
+  res.json({ auth: false, token: null });
+});
+
 app.get('/', (req, res) => {
   res.json({ message: 'API online!', version: '1.0.1' });
 });
 
-app.use('/usuarios', rotasUsuario);
+app.use('/usuarios', verificaJWT, rotasUsuario);
 
 app.use(function (req, res) {
   res.status(404).json({
